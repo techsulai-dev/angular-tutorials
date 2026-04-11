490ca7a44b70cfe0f4ad79e65289ca368c542f1f
techsulai-dev <techsulai786@gmail.com>
2026-04-11 16:44:02 +0530

    just home added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,13 @@ import { Signaldemo } from './Components/signaldemo/signaldemo';
 import { Ngdocheckdemo } from './Components/ngdocheckdemo/ngdocheckdemo';
 import { form } from '@angular/forms/signals';
 import { FormPage } from './Pages/form-page/form-page';
+import { Home } from './Pages/home/home';
 // import { Form } from './Components/form/form';
 
 export const routes: Routes = [
-    { path: '', component: Parentdemo },
+    { path: '', component: Home },
+    { path: 'parent', component: Parentdemo },
+    // { path: '', component: Home },
     { path: 'signal',component: Signaldemo},
     { path: 'reactive-forms', loadComponent: () => import('./Components/reactive-forms/reactive-forms').then(m => m.ReactiveForms) },
    // { path: 'ngdocheckdemo', loadComponent: () => import('./Components/ngdocheckdemo/ngdocheckdemo').then(m => m.Ngdocheckdemo) }
